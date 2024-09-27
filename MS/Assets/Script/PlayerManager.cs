48f33ff0c4dc79d38b223316e751d1d85ee47862
DESKTOP-1PSHM9A\Administrator <atroriass@gmail.com>
2024-09-27 16:58:53 +0900

    first try

---
+++ MS/Assets/Script/PlayerManager.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerManager : MonoBehaviour
+{
+
+    Rigidbody rigidbody;
+
+    Transform cameraTransform;
+
+
+    [SerializeField]
+    float walkSpeed = 0.0f;
+    [SerializeField]
+    float rotateSpeed = 1.0f;
+
+
+    float currentSpeed;
+    float targetSpeed;
+
+
+    Vector2 moveInput;
+    Vector3 playerMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        
+    }
+
+    private void Awake()
+    {
+        cameraTransform = Camera.main.transform;    
+    }
+
+    public void GetMoveInput(InputAction.CallbackContext ctx)
+    {
+        moveInput = ctx.ReadValue<Vector2>();
+        playerMovement.x = moveInput.x;
+        playerMovement.z = moveInput.y;
+
+        Debug.Log(playerMovement);
+    }
+
+    private void FixedUpdate()
+    {
+       
+        Move();
+        Rotate();
+    }
+
+
+    private void Move() { 
+        targetSpeed = walkSpeed;
+        targetSpeed *= moveInput.magnitude;
+        currentSpeed = Mathf.Lerp(currentSpeed, targetSpeed, 0.5f*Time.deltaTime);
+        rigidbody.velocity = playerMovement * currentSpeed;
+    }
+
+    private void Rotate()
+    {
+      if(moveInput.Equals(Vector2.zero))
+          return;
+
+    
+
+        Quaternion targetRotation = Quaternion.LookRotation(playerMovement, Vector3.up);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotateSpeed*Time.deltaTime);
+    }
+
+
+
+ 
+}
