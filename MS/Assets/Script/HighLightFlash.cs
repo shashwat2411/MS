f5f20639645ec616b80e6018d65cd836c1bed881
DESKTOP-1PSHM9A\Administrator <atroriass@gmail.com>
2024-10-22 10:52:23 +0900

    hurt flash

---
+++ MS/Assets/Script/HighLightFlash.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+
+ public class HighLightFlash : MonoBehaviour
+ {
+    public bool start=false;
+    public float flashSpeed;
+     public float intensity;
+    Color nullColor;
+     public Color startColor;
+     public Color endColor;
+     public Material mat;
+     private Material matInstance;
+
+ 
+     private void OnEnable()
+     {
+         
+         matInstance = new Material(mat);
+         matInstance.EnableKeyword("_EMISSION");
+     
+         GetComponent<Renderer>().material = matInstance;
+     }
+
+     private void Update()
+     {
+        if(start)
+            matInstance.SetColor("_EmissionColor", Color.Lerp(nullColor, endColor * intensity, Mathf.PingPong(Time.time, flashSpeed)));
+        else
+            matInstance.SetColor("_EmissionColor", nullColor);
+
+
+    }
+
+     private void OnDisable()
+     {
+         Destroy(matInstance);
+     }
+ }
