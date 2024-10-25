a1e0fb5f45e42ee240b69fe5a1d6515bc33c2af2
DESKTOP-1PSHM9A\Administrator <atroriass@gmail.com>
2024-10-25 17:24:21 +0900

    hurt flash by emission

--- MS/Assets/Script/EnemyBase.cs
+++ MS/Assets/Script/EnemyBase.cs
@@ -7,11 +7,18 @@ public class EnemyBase : MonoBehaviour
     float hp = 100f;
     float maxHp = 100f;
 
+
+
     private HealthBar healthBar;
 
+    [SerializeField]
+    private HighLightFlash highLightFlash;
+
+    private Coroutine hurtFlashCoroutine;
     void Start()
     {
         healthBar = GetComponentInChildren<HealthBar>();
+        highLightFlash = GetComponent<HighLightFlash>();
     }
 
     void FixedUpdate()
@@ -21,11 +28,26 @@ public class EnemyBase : MonoBehaviour
 
     public void Damage(float value)
     {
+        hp-= value;
         healthBar.Damage(value);
+
+        if (this.hurtFlashCoroutine != null)
+        {
+            StopCoroutine(this.hurtFlashCoroutine);
+        }
+
+        if (hp <= 0)
+        {
+            Death();
+        }
+        else
+        {
+            this.hurtFlashCoroutine = StartCoroutine(highLightFlash.HurtFlash());
+        }
     }
 
     public void Death()
     {
-
+        highLightFlash.Death();
     }
 }
--- MS/Assets/Script/HighLightFlash.cs
+++ MS/Assets/Script/HighLightFlash.cs
@@ -10,8 +10,7 @@ using UnityEngine;
     public float flashSpeed;
      public float intensity;
     Color nullColor;
-     public Color startColor;
-     public Color endColor;
+     private Color endColor = Color.red;
      public Material mat;
      private Material matInstance;
 
@@ -27,16 +26,44 @@ using UnityEngine;
 
      private void Update()
      {
-        if(start)
-            matInstance.SetColor("_EmissionColor", Color.Lerp(nullColor, endColor * intensity, Mathf.PingPong(Time.time, flashSpeed)));
-        else
-            matInstance.SetColor("_EmissionColor", nullColor);
-
+        //if (start)
+        //{
+        //    matInstance.SetColor("_EmissionColor", Color.Lerp(nullColor, endColor * intensity, Mathf.PingPong(Time.time, flashSpeed)));
+        //}
+        //else
+        //{
+        //    matInstance.SetColor("_EmissionColor", nullColor);
+        //}
 
     }
 
      private void OnDisable()
      {
+        Death();
          Destroy(matInstance);
      }
+
+    public void Death()
+    {
+        matInstance.SetColor("_EmissionColor", Color.black);
+    }
+
+    public IEnumerator HurtFlash()
+    {
+        var tc = endColor * intensity;
+        float x = 1.0f;
+        float colorValue = tc.r / x;
+        matInstance.SetColor("_EmissionColor", tc);
+        yield return 0;
+        while (tc.r > 0)
+        {
+            x += flashSpeed * Time.deltaTime;
+            tc.r = tc.r > 0.02? tc.r / x : 0;
+            matInstance.SetColor("_EmissionColor", tc);
+            
+            yield return 0;
+        }
+
+    }
+
  }
