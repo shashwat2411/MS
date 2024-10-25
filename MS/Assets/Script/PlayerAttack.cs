d5b3bbe4bafd0874b671098eca4abd4c23197a51
DESKTOP-1PSHM9A\Administrator <atroriass@gmail.com>
2024-10-25 18:48:39 +0900

    hold attack 1.0

--- MS/Assets/Script/PlayerAttack.cs
+++ MS/Assets/Script/PlayerAttack.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
 
 public class PlayerAttack : MonoBehaviour
 {
@@ -9,6 +10,8 @@ public class PlayerAttack : MonoBehaviour
     public float attackTime = 0.1f;
     public float damage = 10f;
     public GameObject collider;
+    public float maxAttackSize = 2.5f;
+
 
     [Header("Long Range")]
     public float cooldown = 0.1f;
@@ -19,10 +22,23 @@ public class PlayerAttack : MonoBehaviour
     public InputActionReference closeRangeAttack;
     public InputActionReference longRangeAttack;
 
+    float holdtime = 1.0f;
+    bool isHold = false;
+    
+
+    float holdIncreaseFlag = 1.0f;
+
+    
     void Start()
     {
-        closeRangeAttack.action.started += CloseRangeAttack;
+        closeRangeAttack.action.started += HoldAttack;
         longRangeAttack.action.started += LongRangeAttack;
+
+
+
+        closeRangeAttack.action.canceled += AttackFinish;
+
+
 
         ResetCollider();
     }
@@ -30,8 +46,43 @@ public class PlayerAttack : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        if(isHold)
+        {
+            collider.GetComponent<Transform>().localScale = Vector3.one * holdtime;
+
+            if (holdtime > maxAttackSize || holdtime < 1.0f)
+            {
+                holdIncreaseFlag = -holdIncreaseFlag;
+            }
+           
+            holdtime += Time.deltaTime * holdIncreaseFlag;
+
+           
+        }
+
+      
+    }
+
+
+    void AttackFinish(InputAction.CallbackContext context)
+    {
+        isHold = false;
+        holdtime = 1.0f;
+
+
+        Invoke("ResetCollider", attackTime);
+
+        //collider.GetComponent<MeshRenderer>().enabled = true;
+        collider.GetComponent<BoxCollider>().enabled = true;
+    }
+
+    void HoldAttack(InputAction.CallbackContext context)
+    {
+        isHold = true;
+        collider.GetComponent<MeshRenderer>().enabled = true;
 
     }
+
 
     void CloseRangeAttack(InputAction.CallbackContext context)
     {
@@ -43,6 +94,8 @@ public class PlayerAttack : MonoBehaviour
     }
     void ResetCollider()
     {
+
+        collider.GetComponent<Transform>().localScale = Vector3.one;
         collider.GetComponent<MeshRenderer>().enabled = false;
         collider.GetComponent<BoxCollider>().enabled = false;
     }
