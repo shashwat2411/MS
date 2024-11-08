0ad228e5fba03e16508bc91ab449f812937ab695
DESKTOP-1PSHM9A\Administrator <atroriass@gmail.com>
2024-11-08 17:21:04 +0900

    prepare to merge

--- MS/Assets/Script/UI/Pause_Time.cs
+++ MS/Assets/Script/UI/Pause_Time.cs
@@ -13,9 +13,9 @@ public class Pause_Time : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            Time.timeScale = 0;
-        }
+        //if (Input.GetKeyDown(KeyCode.Return))
+        //{
+        //    Time.timeScale = 0;
+        //}
     }
 }
