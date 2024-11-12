dc9a5282005a42bdac73773ebc3b7eb527fa7bd3
shashwat2411 <124550087+shashwat2411@users.noreply.github.com>
2024-11-12 09:54:38 +0900

    main fix

--- MS/Assets/Scripts/PlayerManager.cs
+++ MS/Assets/Scripts/PlayerManager.cs
@@ -28,9 +28,10 @@ public class PlayerManager : MonoBehaviour
     Vector3 playerMovement;
     Vector3 playerAttackMovement;
     Vector3 playerMovementWorldSpace;
+    [SerializeField] public Player_HP playerHP;
 
 
-    
+
     [Header("Dash Staff")]
     [SerializeField]
     private bool isDashing = false;
@@ -50,7 +51,6 @@ public class PlayerManager : MonoBehaviour
     Collider collider;
 
     PlayerAttack playerAttack;
-
 
 
     [Header("Player Data Staff")]
@@ -294,6 +294,16 @@ public class PlayerManager : MonoBehaviour
             playerData.exp = playerData.exp + exp;
         }
        
+
+    }
+
+    public void Damage()
+    {
+
+    }
+
+    public void Death()
+    {
 
     }
 }
