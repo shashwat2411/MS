26c25bdb0cf66461919e9755c64f610ab70ef295
Araisan2828 <mirai0927akatuki@gmail.com>
2024-12-08 16:33:11 +0900

    修正

--- MS/Assets/Script/Text/TalkCharacterManager.cs
+++ MS/Assets/Script/Text/TalkCharacterManager.cs
@@ -12,7 +12,7 @@ public class TalkCharacterManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        TalkStart(nowTalker);
     }
 
     // Update is called once per frame
