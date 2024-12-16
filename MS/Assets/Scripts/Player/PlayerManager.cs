c1c7e56c024de0ba5c5b7217fde521b712c5a127
anan9399 <atroriass@gmail.com>
2024-12-16 19:19:16 +0900

    MultiMenko relate to charge level

--- MS/Assets/Scripts/Player/PlayerManager.cs
+++ MS/Assets/Scripts/Player/PlayerManager.cs
@@ -279,7 +279,7 @@ public class PlayerManager : MonoBehaviour
         //playerPrefabs.GetTopItemBonus(BonusSettings.Instance.playerBonusItems[4]);
        //playerHP.Damage(50.0f);
        var test = playerPrefabs.GetTopItemBonus(BonusSettings.Instance.playerBonusItems[0]);
-       playerPrefabs.GetTopItemBonus(BonusSettings.Instance.playerBonusItems[5]);
+       playerPrefabs.GetTopItemBonus(BonusSettings.Instance.playerBonusItems[3]);
 
         //if (!test)
         //{
