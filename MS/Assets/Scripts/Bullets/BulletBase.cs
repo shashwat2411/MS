4e204e3fffe1dc92d359a41225e4b105da5cb33e
shashwat2411 <124550087+shashwat2411@users.noreply.github.com>
2024-12-16 23:40:22 +0900

    Canvas Fix

--- MS/Assets/Scripts/Bullets/BulletBase.cs
+++ MS/Assets/Scripts/Bullets/BulletBase.cs
@@ -63,35 +63,40 @@ public class BulletBase : MonoBehaviour, IAtkEffBonusAdder
         if (once && other.CompareTag("Ground"))
         {
             Debug.Log(other.name);
-            
-            //Position
-            GetComponent<Rigidbody>().velocity = Vector3.zero;
-            this.transform.position = hitPos + Vector3.up * 0.05f;
 
-            //Trail
-            GetComponent<TrailRenderer>().time = 0f;
-
-            //Scale
-            GameObject effect = Instantiate(impactEffect, transform.position, transform.rotation);
-            Vector3 localScale = new Vector3(effectSize, effectSize, effectSize);
-
-            float scale = (this.damage / player.playerData.attack) / player.playerData.maxChargeTime;
-            float finalScale = Mathf.Lerp(0.6f, 1.3f, scale);
-
-            effect.transform.localScale = finalScale * localScale;
-            effect.transform.position = transform.position + new Vector3(0f, -effect.transform.localScale.y / 2f, 0f);
-            //if (scale <= this.maxAttackSize) {  }
-            //else { effect.transform.localScale = this.maxAttackSize * localScale; }
-
-            effect.GetComponent<MenkoExplosion>().damage = scale;
-            effect.GetComponentInChildren<BulletImpact>().damage = this.damage;
-
-            //Invoke Stuff
-            DoSpecialThings();
-            Invoke("DestroyBullet", lifetime);
-            once = false;
+            CollisionProcess();
         }
        
+    }
+
+    private void CollisionProcess()
+    {
+        //Position
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+        this.transform.position = hitPos + Vector3.up * 0.05f;
+
+        //Trail
+        GetComponent<TrailRenderer>().time = 0f;
+
+        //Scale
+        GameObject effect = Instantiate(impactEffect, transform.position, transform.rotation);
+        Vector3 localScale = new Vector3(effectSize, effectSize, effectSize);
+
+        float scale = (this.damage / player.playerData.attack) / player.playerData.maxChargeTime;
+        float finalScale = Mathf.Lerp(0.6f, 1.3f, scale);
+
+        effect.transform.localScale = finalScale * localScale;
+        effect.transform.position = transform.position + new Vector3(0f, -effect.transform.localScale.y / 2f, 0f);
+        //if (scale <= this.maxAttackSize) {  }
+        //else { effect.transform.localScale = this.maxAttackSize * localScale; }
+
+        effect.GetComponent<MenkoExplosion>().damage = scale;
+        effect.GetComponentInChildren<BulletImpact>().damage = this.damage;
+
+        //Invoke Stuff
+        DoSpecialThings();
+        Invoke("DestroyBullet", lifetime);
+        once = false;
     }
 
     void DestroyBullet()
