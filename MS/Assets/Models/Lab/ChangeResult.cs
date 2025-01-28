bf057b229736826ea51886d76091117f7ae1a3cf
shashwat2411 <124550087+shashwat2411@users.noreply.github.com>
2025-01-28 21:06:50 +0900

    boss

--- MS/Assets/Models/Lab/ChangeResult.cs
+++ MS/Assets/Models/Lab/ChangeResult.cs
@@ -2,16 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static System.TimeZoneInfo;
 
 public class ChangeResult : MonoBehaviour
 {
     public ScreenShatter screenShatter;
     public AudioSource source;
+    public PostProcessController postProcess;
     public void MainGame(InputAction.CallbackContext context)
     {
         if (!context.started) return;
 
         source.Play();
-        StartCoroutine(screenShatter.ShatterScreenInitate());
+        StartCoroutine(ScaryEffectOn());
+    }
+    public IEnumerator ScaryEffectOn()
+    {
+        yield return postProcess.ScaryEffectOn(0.2f);
+        yield return SoundEffect(0.2f, 1f, 0.8f);
+
+        yield return screenShatter.ShatterScreenInitate();
+    }
+    private IEnumerator SoundEffect(float duration, float startValue, float endValue)
+    {
+        float elapsed = 0f;
+
+        source.pitch = startValue;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+
+            source.pitch = Mathf.Lerp(startValue, endValue, elapsed / duration);
+
+            yield return null;
+        }
+
+        source.pitch = endValue;
     }
 }
